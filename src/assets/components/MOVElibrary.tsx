a12b0e924acf7f54454134d2d38a0aa5fdf23b5a
gia-grigalashvili <grigalagia07@gmail.com>
2024-06-24 02:40:15 +0400

    add images

--- src/assets/components/MOVElibrary.tsx
+++ src/assets/components/MOVElibrary.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import Data from "/src/data.json";
 function MOVElibrary() {
   return (
     <MainDiv>
       <p>hello</p>
-      <MovieGrid></MovieGrid>
+      <MovieGrid>
+        {Data.map((item, index) => (
+          <MovieDiv
+            key={index}
+            backgroundImage={item.thumbnail.regular?.small}
+          ></MovieDiv>
+        ))}
+      </MovieGrid>
     </MainDiv>
   );
 }
@@ -13,11 +21,31 @@ const MainDiv = styled.div`
   flex-direction: column;
   padding: 20px;
   gap: 20px;
+  p {
+    color: var(--Pure-White, #fff);
+    font-family: Outfit;
+    font-size: 20px;
+    font-weight: 400;
+    letter-spacing: -0.312px;
+  }
 `;
 
 const MovieGrid = styled.div`
   max-width: 164px;
   display: grid;
+  grid-template-columns: 1fr 2fr;
   gap: 20px;
+`;
+const MovieDiv = styled.div`
+  width: 144px;
+  height: 110px;
+
+  align-items: center;
+
+  background-image: url(${(props) => props.backgroundImage});
+  background-size: cover;
+  background-position: center;
+  border-radius: 8px;
+  color: #fff;
 `;
 export default MOVElibrary;
