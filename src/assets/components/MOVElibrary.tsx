d7e287c4e4350155e86b5267e179a0c105b7dd1f
gia-grigalashvili <grigalagia07@gmail.com>
2024-06-25 23:51:14 +0400

    add information

--- src/assets/components/MOVElibrary.tsx
+++ src/assets/components/MOVElibrary.tsx
@@ -1,32 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import Data from "/src/data.json";
+import Data from "/src/data.json"; // Adjust the path to match your project structure
+import Movies from "/public/assets/icon-nav-movies.svg";
+import bookmarkempty from "/public/assets/icon-bookmark-empty.svg";
+
 function MOVElibrary() {
+  const [Moviedata, setMoviedata] = useState(Data.slice(5)); // Initialize state with Data excluding the first five items
+
   return (
     <MainDiv>
       <p>hello</p>
       <MovieGrid>
-        {Data.map((item, index) => (
-          <MovieDiv
-            key={index}
-            backgroundImage={item.thumbnail.regular?.small}
-          ></MovieDiv>
+        {Moviedata.map((item, index) => (
+          <div key={index}>
+            <MovieDiv backgroundImage={item.thumbnail?.regular?.small}>
+              <div className="bookmark">
+                <img src={bookmarkempty} alt="" />
+              </div>
+            </MovieDiv>
+            <Information>
+              <p>{item.year}</p>
+              <img src={Movies} alt="" />
+              <p>{item.category}</p>
+              <p>{item.rating}</p>
+            </Information>
+            <h1>{item.title}</h1>
+          </div>
         ))}
       </MovieGrid>
     </MainDiv>
   );
 }
+
 const MainDiv = styled.div`
   display: flex;
   flex-direction: column;
+
   padding: 20px;
   gap: 20px;
   p {
     color: var(--Pure-White, #fff);
     font-family: Outfit;
-    font-size: 20px;
+    font-size: 11px;
+    font-style: normal;
     font-weight: 400;
-    letter-spacing: -0.312px;
+    line-height: normal;
   }
 `;
 
@@ -35,17 +53,47 @@ const MovieGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 2fr;
   gap: 20px;
+  h1 {
+    color: var(--Pure-White, #fff);
+    font-family: Outfit;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin-top: 4px;
+  }
 `;
+
 const MovieDiv = styled.div`
   width: 144px;
   height: 110px;
-
-  align-items: center;
-
   background-image: url(${(props) => props.backgroundImage});
   background-size: cover;
   background-position: center;
   border-radius: 8px;
   color: #fff;
+  .bookmark {
+    margin-left: 120px;
+
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    width: 22px;
+    height: 22px;
+    border-radius: 50px;
+    background-color: gray;
+  }
+`;
+
+const Information = styled.div`
+  display: flex;
+  align-items: center;
+
+  gap: 5px;
+  img {
+    width: 10px;
+    height: 10px;
+  }
 `;
 export default MOVElibrary;
